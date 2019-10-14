94949143ee0739c9d0cb1689872f1ba7ee505cb0
Eduardo <edumadrim@gmail.com>
2019-10-13 21:24:01 -0600

    MainWindow created & Main added

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import gui.MainWindow;
+
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("University-DS-Case_11");
+        frame.setContentPane(new MainWindow().getMainPanel());
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        //frame.pack();
+        frame.setVisible(true);
+    }
+}
---
+++ src/gui/MainWindow.java
@@ -0,0 +1,11 @@
+package gui;
+
+import javax.swing.*;
+
+public class MainWindow {
+    private JPanel mainPanel;
+
+    public JPanel getMainPanel() {
+        return mainPanel;
+    }
+}
