9bd04a2f809ad2235f09fb13b28568589a9618e1
Gabriel Vargas <gvargasr@estudiantec.cr>
2019-10-15 23:29:11 -0600

    Added JsonManager
    Singleton class

---
+++ src/model/JsonManager.java
@@ -0,0 +1,93 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+import org.json.simple.JSONObject;
+import org.json.simple.JSONArray;
+import org.json.simple.parser.*;
+
+
+public class JsonManager implements IConstants{
+	
+	private static JsonManager singleton;
+	private int width;
+	private int depth;
+	private ArrayList<String> urls;
+	
+	private JsonManager() {
+		this.urls = new ArrayList<String>();
+		try {
+			this.loadJson(JSON_PATH);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public static JsonManager getInstance() {
+		if (singleton == null) {
+			singleton = new JsonManager();
+		}
+		return singleton;
+	}
+	
+	private void loadJson(String pPath) throws IOException {
+		
+		
+		
+		try {
+			JSONObject json = (JSONObject) new JSONParser().parse(new FileReader(pPath));
+			
+			long jWidth = (long) json.get("anchura");
+			long jDepth = (long) json.get("profundidad");
+			
+			this.width = (int) jWidth;
+			this.depth = (int) jDepth;
+			
+			
+			JSONArray urlArray = (JSONArray) json.get("urls");
+			Iterator jsonArrayIterator = urlArray.iterator();
+			
+			while(jsonArrayIterator.hasNext()) {
+				String url = (String) jsonArrayIterator.next();
+				this.urls.add(url);
+			}
+			
+
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	public int getWidth() {
+		return this.width;
+	}
+
+	public int getDepth() {
+		return this.depth;
+	}
+
+	public ArrayList<String> getUrls() {
+		return this.urls;
+	}
+	
+	public static void main(String[] args) {
+		JsonManager jm = JsonManager.getInstance();
+		System.out.println(jm.getDepth());
+		System.out.println(jm.getWidth());
+		System.out.println(jm.getUrls());
+	}
+	
+}
+
