04afa12563cab981c4bcf0afd63041f96cfa2719
Gabriel Vargas <gvargasr@estudiantec.cr>
2019-10-21 04:17:21 -0600

    Fixed relative path to json

--- src/model/IConstants.java
+++ src/model/IConstants.java
@@ -1,5 +1,5 @@
 package model;
 
 public interface IConstants {
-	public static final String JSON_PATH = "/Users/gabriel/Documents/GitHub/University-DS-Case-11/Test_info/urls.json";
+	public static final String JSON_PATH = "Test_info/urls.json";
 }
