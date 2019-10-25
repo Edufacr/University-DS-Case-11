6d482964c3e01df04d960e3c695109649b5cef12
Eduardo <edumadrim@gmail.com>
2019-10-25 17:58:37 -0600

    Delete form UI

--- src/gui/MainWindow.java
+++ src/gui/MainWindow.java
@@ -1,47 +1,4 @@
 package gui;
 
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
 public class MainWindow {
-    private JPanel mainPanel;
-    private JTextField textField;
-    private JButton searchWordsButton;
-    private JButton searchFrequencyButton;
-    private JButton searchDomainButton;
-    private JPanel mainScrollPanel;
-    private JScrollPane mainScroll;
-
-    public MainWindow() {
-        mainScrollPanel.setLayout(new GridLayout());
-        searchDomainButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent actionEvent) {
-                test();
-            }
-        });
-        searchFrequencyButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent actionEvent) {
-
-            }
-        });
-        searchWordsButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent actionEvent) {
-
-            }
-        });
-    }
-
-    public JPanel getMainPanel() {
-        return mainPanel;
-    }
-    private void test(){
-        JLabel testy = new JLabel("Search Result");
-        mainScrollPanel.add(testy);
-        mainScrollPanel.revalidate();
-    }
 }
