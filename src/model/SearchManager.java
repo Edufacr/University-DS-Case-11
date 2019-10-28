5a5924f70046c3fbb1031c99c79e066b2fa8ef9b
Eduardo <edumadrim@gmail.com>
2019-10-28 10:13:35 -0600

    SearchRange

--- src/model/SearchManager.java
+++ src/model/SearchManager.java
@@ -11,8 +11,9 @@ public class SearchManager extends Observable {
     AVLTree<Word> wordTree;
     WebLoader loader;
 
-    public SearchManager(AVLTree<Word> pWordTree){
+    public SearchManager(AVLTree<Word> pWordTree, BPlusTree<Integer,ArrayList<String>> pOccurrenceTree){
         wordTree = pWordTree;
+        wordsOccurrenceTree = pOccurrenceTree;
     }
 
     public void searchWord(String pText){
@@ -33,7 +34,6 @@ public class SearchManager extends Observable {
             }
             setChanged();
             notifyObservers(getIntersections(retList));
-            System.out.println(getIntersections(retList));
         }
     }
     private String[] parseWords(String pText){
@@ -52,7 +52,6 @@ public class SearchManager extends Observable {
             }
             return ret;
         }
-
     }
     private ArrayList<String> intersection(ArrayList<String> pList1, ArrayList<String> pList2){
         ArrayList<String> ret = new ArrayList<String>();
@@ -65,24 +64,58 @@ public class SearchManager extends Observable {
         return ret;
     }
     public void searchRange(String pText){
-
+        if(!pText.isEmpty()){
+            String[] numbers = parseWords(pText);
+            if(numbers.length >= 2){
+                int lowerKey = Integer.parseInt(numbers[0]);
+                int highKey = Integer.parseInt(numbers[1]);
+                ArrayList<String> retList = getUnion(wordsOccurrenceTree.searchKeyRange(lowerKey,highKey)); //getUnion
+                setChanged();
+                notifyObservers(retList);
+            }
+        }
+    }
+    private ArrayList<String> getUnion(ArrayList<ArrayList<String>> pList){
+        if(pList.contains(null)){
+            return new ArrayList<String>();
+        }
+        else{
+            ArrayList<String> ret = pList.get(0);
+            for (int pListIndex = 1; pListIndex < pList.size();pListIndex++){
+                ret = union(ret,pList.get(pListIndex));
+            }
+            return ret;
+        }
+    }
+    private ArrayList<String> union(ArrayList<String> pList1, ArrayList<String> pList2){
+        ArrayList<String> ret = new ArrayList<String>(pList1);
+        for (String word:
+                pList2) {
+            if(ret.contains(word)){
+                continue;
+            }
+            ret.add(word);
+        }
+        return ret;
     }
     public void searchDomain(String pText){
         
     }
     public static void main(String[] args) {
         AVLTree<Word> wordTree = new AVLTree<Word>();
-        ArrayList<String> s1 = new ArrayList<String>();
-        ArrayList<String> s2 = new ArrayList<String>();
-        s1.add("A");
-        s1.add("B");
-        s2.add("A");
-        s2.add("B");
-        wordTree.add(new Word("hola",s1));
-        wordTree.add(new Word("buenas",s2));
-        wordTree.add(new Word("adios","C"));
-        SearchManager manager = new SearchManager(wordTree);
-        manager.searchWord("Hola lol");
+        BPlusTree<Integer, ArrayList<String>> tree = new BPlusTree<Integer, ArrayList<String>>(4);
+        ArrayList<String> uno = new ArrayList<String>();
+        uno.add("hola");
+        uno.add("adios");
+        uno.add("jeje");
+        ArrayList<String> dos = new ArrayList<String>();
+        dos.add("hello");
+        dos.add("bye");
+        dos.add("hehe");
+        tree.add(2,dos);
+        tree.add(1,uno);
+        SearchManager manager = new SearchManager(wordTree,tree);
+
     }
 
 }
